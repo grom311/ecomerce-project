6ccd6cc8f0b688bbe912d6411204d054e4e0cddc
grom311 <grom311@yandex.ru>
2021-01-24 23:35:46 +0300

    categories

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -13,9 +14,12 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
